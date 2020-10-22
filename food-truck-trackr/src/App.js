fa1972a0098d870c88e8b14cbed7e155870b02c3
Samantha Goodman <slgoodman89@gmail.com>
2020-10-21 22:33:39 -0400

    edit app.js

--- food-truck-trackr/src/App.js
+++ food-truck-trackr/src/App.js
@@ -11,23 +11,12 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>Food Truck Trackr</h1>
+        <p>Connecting foodies to food trucks in seconds!</p>
       </header>
-      <DinerLogin />
-      <DinerRegistration />
       <Navigation />
-      <Home />
+      <DinerLogin />
+      <DinerRegistration />  
     </div>
   );
 }
