bed699f4c7dadf7e4a57200a418375cf0f9bdf7b
Samantha Goodman <antashma@gmail.com>
2020-10-24 22:53:48 -0400

    fixed axiosWithAuth

--- food-truck-trackr/src/api/axiosWithAuth.js
+++ food-truck-trackr/src/api/axiosWithAuth.js
@@ -4,7 +4,7 @@ export const axiosWithAuth = () => {
     const token = localStorage.getItem('token');
     return axios.create({
         headers: {
-            Authorization: token, 
+            Authorization: `Bearer ${token}`, 
         },
         baseURL: 'https://build-week-food-truck.herokuapp.com/api'
     })
